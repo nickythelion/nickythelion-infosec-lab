5447b165ca274d257afc2e41f9e82104e040fd0f
nickythelion <nickythelionfurry@yandex.ru>
2022-11-01 21:43:12 +0300

    Updated CMD1.bat

--- Lab 1/CMD1.bat
+++ Lab 1/CMD1.bat
@@ -6,4 +6,13 @@ if EXIST cmd_1_output.txt (
    echo Output file already exists. Overwriting it...
    del cmd_1_output.txt
 )
-help > cmd_1_output.txt
+
+echo. > cmd_1_output.txt
+
+set cmd_list=CD, DIR, CLS, COPY, FC, DEL, ECHO, MD, SET, SORT, GOTO, IF, MOVE, MORE, PATH, PAUSE, RD, REM, REN, REPLACE, EXIT, START, SUBST, TYPE
+for %%i in (%cmd_list%) do (
+   echo ====== %%i >> cmd_1_output.txt
+   %%i /? >> cmd_1_output.txt
+   echo. >> cmd_1_output.txt
+   echo %%i
+)
